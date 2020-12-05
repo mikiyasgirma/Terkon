351ac25802ea94a1b18ae884826aa6343e16ca3c
miko <mikiyasgirma43@gmail.com>
2020-12-04 16:31:04 -0800

    test_01

--- functions/index.js
+++ functions/index.js
@@ -10,5 +10,5 @@ const cors = require('cors');
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.send("Hello from Firebases!");
 });
